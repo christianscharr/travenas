180c29328321e0b65b63240e6d64c857ad9bd01f
Garrett Wang <garrettzhixin.wang@css.ch>
2017-09-16 06:39:23 +0200

    return a json object with chat channel url

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var SendBird = require('sendbird');
+//var Map = require('Map');
 var chat = express.Router();
 
-var chatRooms = [];
+var chatRooms = new Map();
 const APP_ID = 'F5382E02-4083-4C36-99CC-E4181584CDD8';
 const CHAT_ROOM_ROUTE = 'ChatRoomRoute_';
-const CHAT_ROOM_ROUTE_URL = 'ChatRoomRouteUrl_';
+const CHAT_ROOM_ROUTE_URL = 'ChatRoomRouteUrl';
 const CHAT_ROOM_STATION = 'ChatRoomStation_';
-const CHAT_ROOM_STATION_URL = 'ChatRoomStationUrl_';
+const CHAT_ROOM_STATION_URL = 'ChatRoomStationUrl';
 
 const sb = new SendBird({'appId': APP_ID});
 
 chat.get('/', function (req, res) {
 
+
     const routeId = req.query.routeId;
     const stationId = req.query.stationId;
+    const result = {};
 
     sb.connect('admin', (user, error) => {
         if(error) {
@@ -22,13 +25,16 @@ chat.get('/', function (req, res) {
         } else {
             console.log('sendbird is connected by admin from server');
             sb.OpenChannel.createChannel(CHAT_ROOM_ROUTE + routeId,
-                                        CHAT_ROOM_ROUTE_URL + routeId,
+                                        CHAT_ROOM_ROUTE_URL + '_' + routeId,
                                         'data...', (createdChannel, error) => {
                 if (error) {
                     console.error(error);
                     return;
                 }
-                createChannelForStation(stationId, res);
+                console.log('url of createdChannel ' + CHAT_ROOM_ROUTE + ' ', createdChannel.url);
+                result[CHAT_ROOM_ROUTE_URL] = createdChannel.url;
+                chatRooms.set()
+                createChannelForStation(stationId, res, result);
             });
         }
 
@@ -36,15 +42,16 @@ chat.get('/', function (req, res) {
 
 });
 
-function createChannelForStation(stationId, res) {
+function createChannelForStation(stationId, res, result) {
     sb.OpenChannel.createChannel(CHAT_ROOM_STATION+ stationId,
-                                CHAT_ROOM_STATION_URL+ stationId,
+                                CHAT_ROOM_STATION_URL + '_'+ stationId,
                                 'data...', (createdChannel, error) => {
         if (error) {
             console.error(error);
             return;
         }
-        res.send('ok');
+        result[CHAT_ROOM_STATION_URL] = createdChannel.url;
+        res.send(result);
 
         // onCreated
         console.log('createdChannel', createdChannel);
