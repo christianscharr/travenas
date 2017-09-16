49af35ca1b6e731688fc51e1a83102f3189bd225
reglid <dominik@regli.info>
2017-09-16 03:20:42 +0200

    Added initial impl to connections API (viadi)

---
+++ backend/routes/connections.js
@@ -0,0 +1,10 @@
+var controller = require('../controllers/ConnectionsController');
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  controller.getConnection(req.query.from, req.query.to).then(function (value) { res.send(value) });
+});
+
+module.exports = router;
